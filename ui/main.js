d8b7596003b7b1ffd728a0f714a36d1c7264faed
JVElizabeth <jehovaseema97@gmail.com>
2017-03-18 20:15:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,8 @@
 console.log('Loaded!');
 var button=document.getElementById("counter");
-
+var counter=0;
 button.onclick=function(){
+    counter=counter+1;
   var request=new XMLHttpRequest();
    request.onreadystatechanged=function(){
     if(request.readystate==XMLHttpRequest.DONE){
