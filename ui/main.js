a7c410080cfe3d6c51a0cef867bea61da6949018
JVElizabeth <jehovaseema97@gmail.com>
2017-03-18 13:46:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,7 @@
 console.log('Loaded!');
 
 var img=document.getElementById("madi");
-var marginLeft=0;
-function moveRight(){
-    marginLeft = merginLeft + 10;
-    img.style.marginLeft+'px';
-}
+
 img.onclick=function(){
-img.interval=setInterval(moveRight,100);
+img.style.marginLeft('100px');
 };
