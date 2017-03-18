85eb1271c663d916cf0759d29549fa32bb3592e0
JVElizabeth <jehovaseema97@gmail.com>
2017-03-18 19:37:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,14 +9,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.send('Article one requested and will be served here');
-});
-app.get('/article-two', function (req, res) {
-  res.send('Article two requested and will be served here');
-});
-app.get('/article-three', function (req, res) {
-  res.send('Article three requested and will be served here');
+var counter=0;
+app.get('/counter',function(req,res){
+    counter=counter+1;
+   res.send(counter.toString()); 
+
 });
 
 
