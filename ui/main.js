5f2e3767dff61909501394ef2c4adfdb7a970079
JVElizabeth <jehovaseema97@gmail.com>
2017-03-18 20:08:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,19 @@
 console.log('Loaded!');
-
 var button=document.getElementById("counter");
 var counter=0;
 button.onclick=function(){
-  counter=counter+1; 
-  var span=document.getElementById("count");
-  span.innerHTML=counter.toString();
+  var request=new XMLHttpRequest();
+   request.onreadystatechanged=function(){
+    if(request.readystate==XMLHttpRequest.DONE){
+        if(request.status==200){
+             var counter=requset.responseText;
+             var span=document.getElementById("count");
+             span.innerHTML=counter.toString();
+        }
+    }
+
+};
+   request.open('GET','http://jvelizabeth.imad.hasura-app.io/',true);
+   request.send(null);
+ 
 };
